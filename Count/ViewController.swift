ea7e73a8315fcde0208e94517530101f51bd0373
NeX0Zz <den.nickol1406@gmail.com>
2023-11-12 21:45:23 +0300

    Готовый вариант

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -1,19 +1,88 @@
 //
 //  ViewController.swift
-//  Count
+//  Counter
 //
-//  Created by Mac on 2023-11-12.
+//  Created by Mac on 2023-11-11.
 //
-
+import Foundation
 import UIKit
-
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pressButton: UIButton!
+    @IBOutlet weak var textLableCount: UILabel!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var changeView: UITextView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    func getDate() -> String{
+        let getDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        let qwe = dateFormatter.string(from: getDate)
+        return qwe
+    }
+    var count = 0{
+        didSet{
+            if count < 0 {
+                count = 0
+            }
+        }
+    }
+    var updateTextView:String = """
+История изменений:
 
+"""
+    @IBAction func pressButton(_ sender: Any) {
+        if count >= 0{
+            count += 1
+        }
+            textLableCount.text = "Значение счётчика: \(count)"
+        changeView.text = updateTextView
+        }
+    
+    @IBAction func redButton(_ sender: Any) {
+        count += 1
+        textLableCount.text = "Значение счётчика: \(count)"
+        updateTextView += """
+\(getDate())
+Значение изменено на +1
+
+"""
+        changeView.text = updateTextView
+    }
+    @IBAction func blueButton(_ sender: Any) {
+        if count == 0{
+            updateTextView += """
+    \(getDate())
+    Попытка уменьшить значение счётчика ниже 0
+    
+    """
+        }else{
+            count -= 1
+            textLableCount.text = "Значение счётчика: \(count)"
+            updateTextView += """
+        \(getDate())
+        Значение изменено на -1
+        
+        """
+            
+        }
+        changeView.text = updateTextView
+    }
+    @IBAction func resetButton(_ sender: Any) {
+        count = 0
+        textLableCount.text = "Значение счётчика: \(count)"
+        updateTextView += """
+\(getDate())
+Значение сброшено
+
+"""
+        changeView.text = updateTextView
+    }
 
 }
 
